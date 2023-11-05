41f89ab83386b498f11424134d69745c2ddb595d
firstnextt <alabuzhev.danil@mail.ru>
2023-11-06 02:38:05 +0300

    connect mongo db and create schema

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from '@nestjs/mongoose'
+
+
+@Module({
+    imports: [MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.2v6dsac.mongodb.net/?retryWrites=true&w=majority'),
+        TrackModule]
+})
+export class AppModule {}
